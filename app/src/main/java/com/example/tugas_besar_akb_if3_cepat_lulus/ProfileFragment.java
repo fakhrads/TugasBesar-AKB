d07163c5e2ce3d14dbd681215a8ef21ab172a245
FauziSS123 <muhammadrojabinuralbishri@gmail.com>
2022-08-03 12:03:48 +0700

    create profile

--- app/src/main/java/com/example/tugas_besar_akb_if3_cepat_lulus/ProfileFragment.java
+++ app/src/main/java/com/example/tugas_besar_akb_if3_cepat_lulus/ProfileFragment.java
@@ -1,5 +1,10 @@
 package com.example.tugas_besar_akb_if3_cepat_lulus;
-
+// nama  : muhammad rojabi nur fauzi
+// nim   : 10119091
+// kelas : if_3
+// nama  : fauzi fikrullah
+// nim   : 10119111
+// kelas : if_3
 import android.content.Intent;
 import android.os.Bundle;
 
@@ -28,27 +33,36 @@ public class ProfileFragment extends Fragment implements AdapterView.OnItemClick
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        String[] profileName = {"Oji","Pukri","Jikun"};
+        String[] profileName = {"", "Fauzi Fikrullah", "Muhammad Rojabi Nur Fauzi", "Fakhri Adi Saputra", "Fikri Muhamad Fauzi", "Ahmad Fikri Maulana"};
 
-        ListView listView = (ListView)view.findViewById(R.id.list_v);
+        ListView listView = (ListView)view.findViewById(R.id.list_profile);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, profileName);
         listView.setAdapter(adapter);
+
         listView.setOnItemClickListener(this);
 
     }
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-        if(position==0) {
-            Intent intent = new Intent(getActivity().getApplication(), OjiActivity.class);
-            startActivity(intent);
-        }
         if(position==1) {
             Intent intent = new Intent(getActivity().getApplication(), OjiActivity.class);
             startActivity(intent);
         }
         if(position==2) {
-            Intent intent = new Intent(getActivity().getApplication(), OjiActivity.class);
+            Intent intent = new Intent(getActivity().getApplication(), FauziActivity.class);
+            startActivity(intent);
+        }
+        if(position==3) {
+            Intent intent = new Intent(getActivity().getApplication(), FakhriActivity.class);
+            startActivity(intent);
+        }
+        if(position==4) {
+            Intent intent = new Intent(getActivity().getApplication(), FikriActivity.class);
+            startActivity(intent);
+        }
+        if(position==5) {
+            Intent intent = new Intent(getActivity().getApplication(), AhmadActivity.class);
             startActivity(intent);
         }
     }
